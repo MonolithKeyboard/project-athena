0c67d671788596dc9cd08adcec7aa38f6a856342
Eustathios Aravopoulos <steelfree6@gmail.com>
2024-06-23 13:47:16 +0300

    добавил karma.conf.js

---
+++ athena/karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-firefox-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // оставляет результат теста видимым в браузере
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['FirefoxHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
